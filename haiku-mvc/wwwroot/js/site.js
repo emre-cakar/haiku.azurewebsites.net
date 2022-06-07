89529c81a965a3afae1c52c4d2c3126f0fb9210b
FilizYalcin21 <filizyalcin@marun.edu.tr>
2022-06-07 23:52:24 +0300

    update
    JavaScript ile ayarlar sayfası geçişleri eklendi

--- haiku-mvc/wwwroot/js/site.js
+++ haiku-mvc/wwwroot/js/site.js
@@ -2,3 +2,16 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+function openAttachment() {
+  document.getElementById('attachment').click();
+}
+
+function fileSelected(input){
+  document.getElementById('btnAttachment').value = "Resim : " + input.files[0].name
+}
+
+function gizleGoster(clickedID){
+  const obj = document.querySelector(clickedID);
+  obj.replace('d-none', 'd-block');
+}
